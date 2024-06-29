a49145105dc4494cef65591ae586ef5ad751dac8
Mark Mihalik <m2@m2home.mynetworksettings.com>
2024-06-29 15:28:54 -0400

    feat: initial implementation
    fix: fix build.yml
         correct cosign-action name
         use ENV instead of outputs
         missing variable
         correct akmods tag

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# m2os
+# m2Giles' OS
+
+This is my customized version of universal blue's bluefin/aurora and bazzite for my needs.
+
+## Additions
+
+This adds the following packages:
+- Emacs
+- Sunshine on Bazzite Build
+- Bootc on Bazzite Build
+
+This adds the following items:
+- Assemble Files for distrobox docker and distrobox incus
---
+++ build.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+
+# ZFS until this gets merged upstream
+if [[ "${IMAGE}" =~ (bluefin|aurora) ]]; then
+    curl -L -o /etc/yum.repos.d/fedora-coreos-pool.repo \
+        https://raw.githubusercontent.com/coreos/fedora-coreos-config/testing-devel/fedora-coreos-pool.repo
+    KERNEL_FOR_DEPMOD="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+    rpm-ostree install /tmp/akmods-rpms/kmods/zfs/*.rpm pv
+    depmod -A "${KERNEL_FOR_DEPMOD}"
+fi
+
+# Bazzite Changes
+if [[ "${IMAGE}" == "bazzite-gnome-nvidia" ]]; then
+    rpm-ostree install \
+        sunshine \
+        bootc
+    systemctl enable sunshine-workaround.service
+    curl -Lo /usr/libexec/ublue-bling.sh \
+        https://raw.githubusercontent.com/ublue-os/bluefin/main/system_files/shared/usr/libexec/ublue-bling.sh
+    chmod +x /usr/libexec/ublue-bling.sh
+    mkdir -p /usr/share/ublue-os/{bluefin-cli,homebrew}
+    curl -Lo /usr/share/ublue-os/homebrew/bluefin-cli.Brewfile \
+        https://raw.githubusercontent.com/ublue-os/bluefin/main/system_files/shared/usr/share/ublue-os/homebrew/bluefin-cli.Brewfile
+    curl -Lo /usr/share/ublue-os/bluefin-cli/bling.sh \
+        https://raw.githubusercontent.com/ublue-os/bluefin/main/system_files/shared/usr/share/ublue-os/bluefin-cli/bling.sh
+    curl -Lo /usr/share/ublue-os/bluefin-cli/bling.fish \
+        https://raw.githubusercontent.com/ublue-os/bluefin/main/system_files/shared/usr/share/ublue-os/bluefin-cli/bling.fish
+    cat >> /usr/share/ublue-os/just/80-bazzite.just <<EOF
+
+# Bluefin-CLI Bling
+bluefin-cli:
+    @/usr/libexec/ublue-bling.sh
+EOF
+fi
+
+# Emacs
+rpm-ostree install emacs
+
+# VSCode because it's still better for a lot of things
+curl -Lo /etc/yum.repos.d/vscode.repo \
+    https://raw.githubusercontent.com/ublue-os/bluefin/main/system_files/dx/etc/yum.repos.d/vscode.repo
+
+rpm-ostree install code
+
+# Docker sysctl.d
+mkdir -p /usr/lib/sysctl.d
+echo "net.ipv4.ip_forward = 1" > /usr/lib/sysctl.d/docker-ce.conf
+sysctl -p
+
+# Distrobox Assemble Files
+curl -Lo /tmp/incus.ini \
+    https://raw.githubusercontent.com/ublue-os/toolboxes/main/apps/incus/distrobox.ini
+
+curl -Lo /tmp/docker.ini \
+    https://raw.githubusercontent.com/ublue-os/toolboxes/main/apps/docker/distrobox.ini
+
+{ printf "\n"; /tmp/incus.ini; printf "\n"; /tmp/docker.ini; } >> /usr/etc/distrobox/distrobox.ini
+
+groupadd -g 250 incus-admin
+groupadd -g 251 incus
+groupadd -g 252 docker
