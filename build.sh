fb322722cf85cd54a47147f91acaced541f7a667
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-07-14 08:23:38 -0400

    feat: add zed

--- build.sh
+++ build.sh
@@ -32,6 +32,16 @@ rpm-ostree install \
     sunshine \
     swtpm
 
+# Zed because why not?
+curl -Lo /tmp/zed.tar.gz \
+    https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz
+mkdir -p /usr/lib/zed.app/
+tar -xvf /tmp/zed.tar.gz -C /usr/lib/zed.app/ --strip-components=1
+ln -s /usr/lib/zed.app/bin/zed /usr/bin/zed
+cp /usr/lib/zed.app/share/applications/zed.desktop /usr/share/applications/dev.zed.Zed.desktop
+sed -i "s|Icon=zed|Icon=/usr/lib/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" /usr/share/applications/dev.zed.Zed.desktop
+sed -i "s|Exec=zed|Exec=/usr/lib/zed.app/libexec/zed-editor|g" /usr/share/applications/dev.zed.Zed.desktop
+
 # Docker sysctl.d
 mkdir -p /usr/lib/sysctl.d
 echo "net.ipv4.ip_forward = 1" > /usr/lib/sysctl.d/docker-ce.conf
