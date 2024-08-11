643da6e8247b9105242c09ef984e9f02f528c4f1
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-08-10 21:06:31 -0400

    use curl, swap config order

--- config.sh
+++ config.sh
@@ -1,33 +1,13 @@
 #!/usr/bin/env sh
 
-tee /etc/yum.repos.d/_copr_ublue-os-staging.repo <<'EOF'
-[copr:copr.fedorainfracloud.org:ublue-os:staging]
-name=Copr repo for staging owned by ublue-os
-baseurl=https://download.copr.fedorainfracloud.org/results/ublue-os/staging/fedora-$releasever-$basearch/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://download.copr.fedorainfracloud.org/results/ublue-os/staging/pubkey.gpg
-repo_gpgcheck=0
-enabled=1
-enabled_metadata=1
-EOF
+curl -Lo /etc/yum.repos.d/_copr_ublue-os_staging.repo \
+    https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-"${FEDORA_VERSION}"/ublue-os-staging-fedora-"${FEDORA_VERSION}".repo
 
-tee /etc/yum.repos.d/_copr_che-nerd-fonts.repo <<'EOF'
-[copr:copr.fedorainfracloud.org:che:nerd-fonts]
-name=Copr repo for nerd-fonts owned by che
-baseurl=https://download.copr.fedorainfracloud.org/results/che/nerd-fonts/fedora-$releasever-$basearch/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://download.copr.fedorainfracloud.org/results/che/nerd-fonts/pubkey.gpg
-repo_gpgcheck=0
-enabled=1
-enabled_metadata=1
-EOF
+curl -Lo /etc/yum.repos.d/_copr_che-nerd-fonts.repo \
+    https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${FEDORA_VERSION}"/che-nerd-fonts-fedora-"${FEDORA_VERSION}".repo
 
-rpm-ostree install /tmp/config-rpms/ublue-os-just.noarch.rpm || true
 rpm-ostree install /tmp/config-rpms/ublue-os-luks.noarch.rpm || true
+rpm-ostree install /tmp/config-rpms/ublue-os-just.noarch.rpm || true
 rpm-ostree install /tmp/config-rpms/ublue-os-signing.noarch.rpm || true
 rpm-ostree install /tmp/config-rpms/ublue-os-udev-rule.noarch.rpm || true
 rpm-ostree install /tmp/config-rpms/ublue-os-update-services.noarch.rpm || true
