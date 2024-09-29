000f45da0702b867e117d3b250903efdc5105eb8
m2Giles <69128853+m2Giles@users.noreply.github.com>
2024-09-29 08:07:21 -0400

    change build for cosmic

--- cosmic.sh
+++ cosmic.sh
@@ -2,6 +2,8 @@
 
 set -eoux pipefail
 
+mkdir -p /var/lib/alternatives
+
 curl -Lo /etc/yum.repos.d/_copr_ryanabx-cosmic.repo \
     https://copr.fedorainfracloud.org/coprs/ryanabx/cosmic-epoch/repo/fedora-"$(rpm -E %fedora)"/ryanabx-cosmic-epoch-fedora-"$(rpm -E %fedora)".repo
 
@@ -15,3 +17,12 @@ systemctl disable gdm || true
 systemctl disable sddm || true
 systemctl enable cosmic-greeter
 systemctl enable power-profiles-daemon
+
+# Clean Up
+mv /var/lib/alternatives /staged-alternatives
+rm -rf /tmp/* || true
+rm -rf /var/* || true
+ostree container commit
+mkdir -p /tmp
+mkdir -p /var/lib/ && mv /staged-alternatives /var/lib/alternatives
+mkdir -p /var/tmp && chmod -R 1777 /var/tmp
