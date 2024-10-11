464b50abac79de820f8a968137c2d9fa93c6b76c
m2 <69128853+m2Giles@users.noreply.github.com>
2024-10-11 10:40:09 -0400

    Use xdg-autostart for ssh-identities

--- desktop-defaults.sh
+++ desktop-defaults.sh
@@ -16,15 +16,21 @@ for IDENTITY in $(find ~/.ssh/*.pub -type f); do
 done
 EOF
 
-tee /tmp/ssh-add-identities-autorun.sh <<'EOF'
-#!/usr/bin/sh
-/usr/libexec/ssh-add-identities
+tee /tmp/ssh-add-identities.desktop<<'EOF'
+[Desktop Entry]
+Exec=/usr/libexec/ssh-add-identities
+Icon=application-x-shellscript
+Name=ssh-add-identities
+Type=Application
+X-KDE-AutostartScript=true
+OnlyShowIn=KDE
 EOF
 
 if [[ ${IMAGE} =~ aurora ]]; then
     cp /tmp/ssh-add-identities /usr/libexec/
     chmod +x /usr/libexec/ssh-add-identities
-    cp /tmp/ssh-add-identities-autorun.sh /etc/profile.d/
+    mkdir -p /etc/xdg/autostart
+    cp /tmp/ssh-add-identities.desktop /etc/xdg/autostart/
 fi
 
 
