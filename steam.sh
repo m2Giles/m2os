2c7f68db437ecdd6eb8d32bd9ab622477751ad28
m2 <69128853+m2Giles@users.noreply.github.com>
2024-10-15 05:24:53 -0400

    Update steam.sh

--- steam.sh
+++ steam.sh
@@ -8,7 +8,6 @@ sed -i "s@enabled=0@enabled=1@" /etc/yum.repos.d/fedora-updates-archive.repo
 
 rpm-ostree override replace \
     --experimental \
-    --from repo=updates \
         systemd \
         systemd-libs \
         systemd-pam \
