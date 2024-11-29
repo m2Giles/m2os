e186d725dfa9256f49b8008c50cfb310773b14d3
m2 <69128853+m2Giles@users.noreply.github.com>
2024-11-29 17:48:46 -0500

    fix ucore dnf5-plugins

--- server-packages.sh
+++ server-packages.sh
@@ -12,8 +12,9 @@ gpgcheck=1
 gpgkey=https://download.docker.com/linux/fedora/gpg
 EOF
 
-# Incus COPR Repo
-dnf5 -y copr enable ganto/lxc4
+# Incus COPR Repo (Ucore does not have python)
+curl -Lo /etc/yum.repos.d/ganto-lxc4-fedora-"$(rpm -E %fedora)".repo \
+        https://copr.fedorainfracloud.org/coprs/ganto/lxc4/repo/fedora-"$(rpm -E %fedora)"/ganto-lxc4-fedora-"$(rpm -E %fedora)".repo
 
 SERVER_PACKAGES=(
     binutils
