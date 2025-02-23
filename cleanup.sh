26b49bed60bcafc6116208224caa841baa382adc
m2 <69128853+m2Giles@users.noreply.github.com>
2025-02-23 16:18:42 -0500

    remove alternatives workaround

--- cleanup.sh
+++ cleanup.sh
@@ -4,28 +4,28 @@
 set ${SET_X:+-x} -eou pipefail
 
 repos=(
-    charm.repo
-    docker-ce.repo
-    fedora-cisco-openh264.repo
-    fedora-updates.repo
-    fedora-updates-archive.repo
-    fedora-updates-testing.repo
-    ganto-lxc4-fedora-"$(rpm -E %fedora)".repo
-    ganto-umoci-fedora-"$(rpm -E %fedora)".repo
-    google-chrome.repo
-    negativo17-fedora-multimedia.repo
-    negativo17-fedora-nvidia.repo
-    nvidia-container-toolkit.repo
-    rpm-fusion-nonfree-nvidia-driver.repo
-    rpm-fusion-nonfree-steam.repo
-    tailscale.repo
-    ublue-os-staging-fedora-"$(rpm -E %fedora)".repo
-    vscode.repo
+    charm
+    docker-ce
+    fedora-cisco-openh264
+    fedora-updates
+    fedora-updates-archive
+    fedora-updates-testing
+    ganto-lxc4-fedora-"$(rpm -E %fedora)"
+    ganto-umoci-fedora-"$(rpm -E %fedora)"
+    google-chrome
+    negativo17-fedora-multimedia
+    negativo17-fedora-nvidia
+    nvidia-container-toolkit
+    rpm-fusion-nonfree-nvidia-driver
+    rpm-fusion-nonfree-steam
+    tailscale
+    ublue-os-staging-fedora-"$(rpm -E %fedora)"
+    vscode
 )
 
 for repo in "${repos[@]}"; do
-    if [[ -f "/etc/yum.repos.d/$repo" ]]; then
-        sed -i 's@enabled=1@enabled=0@g' "/etc/yum.repos.d/$repo"
+    if [[ -f "/etc/yum.repos.d/${repo}.repo" ]]; then
+        sed -i 's@enabled=1@enabled=0@g' "/etc/yum.repos.d/${repo}.repo"
     fi
 done
 
@@ -39,10 +39,10 @@ fi
 
 dnf5 clean all
 
-mv /var/lib/alternatives /staged-alternatives
 rm -rf /tmp/*
 rm -rf /var/*
-ostree container commit
 mkdir -p /tmp
-mkdir -p /var/lib/ && mv /staged-alternatives /var/lib/alternatives
-mkdir -p /var/tmp && chmod -R 1777 /var/tmp
+mkdir -p /var/tmp
+chmod -R 1777 /var/tmp
+
+ostree container commit
