89ce6b164ce410760754c8f2f2849a109ab5ebd0
m2 <69128853+m2Giles@users.noreply.github.com>
2025-03-19 11:33:23 -0400

    fix: multiline sed fix
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/composefs.sh
+++ build_files/composefs.sh
@@ -3,5 +3,5 @@
 set -eoux pipefail
 
 cat /usr/lib/ostree/prepare-root.conf
-sed 's/[composefs]\nenabled = no/[composefs]\nenabled = yes/g' /usr/lib/ostree/prepare-root.conf
+sed -i '{N; s/\[composefs\]\nenabled = no/\[composefs\]\nenabled = yes/g}' /usr/lib/ostree/prepare-root.conf
 cat /usr/lib/ostree/prepare-root.conf
