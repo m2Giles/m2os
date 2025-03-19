45054fbfc8f7e2bc2f1b7510d6a914ad03b0384e
m2 <69128853+m2Giles@users.noreply.github.com>
2025-03-19 13:11:06 -0400

    feat: Add more ComposeFS changes
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/composefs.sh
+++ build_files/composefs.sh
@@ -2,6 +2,15 @@
 
 set -eoux pipefail
 
+# Change the Config File (First Boot Only?)
 cat /usr/lib/ostree/prepare-root.conf
 sed -i '{N; s/\[composefs\]\nenabled = no/\[composefs\]\nenabled = yes/g}' /usr/lib/ostree/prepare-root.conf
 cat /usr/lib/ostree/prepare-root.conf
+
+# Set Compose FS
+ostree config set ex-integrity.composefs yes
+
+# Remove ostree-grub2 if present
+if rpm -q ostree-grub2; then
+    dnf5 remove -y ostree-grub2
+fi
