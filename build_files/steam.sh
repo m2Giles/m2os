c1a8e7e6b3f6b3b7901417c37fc4e3eebc442458
m2 <69128853+m2Giles@users.noreply.github.com>
2025-04-27 09:10:28 -0400

    enable-repo not limit repo
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/steam.sh
+++ build_files/steam.sh
@@ -68,7 +68,7 @@ chmod +x /usr/bin/latencyflex
 mkdir -p /usr/share/gamescope-session-plus
 curl -Lo /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz https://large-package-sources.nobaraproject.org/bootstrap_steam.tar.gz
 dnf5 install -y \
-    --repo copr:copr.fedorainfracloud.org:bazzite-org:bazzite \
+    --enable-repo="copr:copr.fedorainfracloud.org:bazzite-org:bazzite" \
     gamescope-session-plus \
     gamescope-session-steam
 
