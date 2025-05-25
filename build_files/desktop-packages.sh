61de1ae1df042ebd75e8783f4fd5992236e7b80f
m2 <69128853+m2Giles@users.noreply.github.com>
2025-05-24 22:33:23 -0400

    fix: remove evolution
    evolution requires compiling gschema which doesn't appear feasible with sysexts
    
    Signed-off-by: m2 <69128853+m2Giles@users.noreply.github.com>

--- build_files/desktop-packages.sh
+++ build_files/desktop-packages.sh
@@ -85,7 +85,7 @@ install -m 0755 /tmp/ghostty.appimage /usr/bin/ghostty
 
 # Sysexts
 mkdir -p /usr/lib/sysupdate.d
-SYSEXTS=(emacs evolution)
+SYSEXTS=(emacs)
 for s in "${SYSEXTS[@]}"; do
     tee /usr/lib/sysupdate.d/"$s".transfer <<EOF
 [Transfer]
