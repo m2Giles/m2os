18eda8766331453b9073dc7807467601ac1f9139
m2 <69128853+m2Giles@users.noreply.github.com>
2026-03-01 19:08:20 +0000

    fixes

--- build_files/server-packages.sh
+++ build_files/server-packages.sh
@@ -67,7 +67,7 @@ dnf5 swap -y \
     nano-default-editor vim-default-editor
 
 # Incus UI
-curl -Lo /tmp/incus-ui-canonical.deb \
+curl -Lo --retry 7 /tmp/incus-ui-canonical.deb \
     https://pkgs.zabbly.com/incus/stable/pool/main/i/incus/"$(curl https://pkgs.zabbly.com/incus/stable/pool/main/i/incus/ | grep -E incus-ui-canonical | cut -d '"' -f 2 | sort -r | head -1)"
 
 ar -x --output=/tmp /tmp/incus-ui-canonical.deb
