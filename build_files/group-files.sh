6268acafccdf0a381aa6bb1d46dbbde448602d2b
m2Giles <69128853+m2Giles@users.noreply.github.com>
2026-03-05 11:24:33 -0500

    Don't do srpm grouping

--- build_files/group-files.sh
+++ build_files/group-files.sh
@@ -52,47 +52,47 @@ find /usr -regex "\(.*homebrew.*\|.*brew-.*\.\(service\|timer\)\)" | while read 
     setfattr -n user.component -v "brew" "$file"
 done
 
-# Group rpm-ostree rpmdb with rpm rpmdb
+# Tag RPM-Ostree sqlite db
 if [[ -f /usr/lib/sysimage/rpm-ostree-base-db/rpmdb.sqlite ]]; then
     setfattr -n user.component -v "rpm" /usr/lib/sysimage/rpm-ostree-base-db/rpmdb.sqlite
 fi
-for file in $(rpm -q --fileprovide rpm); do
-    [[ "$file" =~ .build-id ]] && continue
-    [[ -e "$file" ]] && setfattr -n user.component -v "rpm" "$file"
-done
+# for file in $(rpm -q --fileprovide rpm); do
+#     [[ "$file" =~ .build-id ]] && continue
+#     [[ -e "$file" ]] && setfattr -n user.component -v "rpm" "$file"
+# done
 
-rawdata="$(rpm -qa --queryformat '%{SOURCERPM}|%{NAME}\n' | grep -v "(none)")"
-json_srpms="$(echo "$rawdata" | jq -R 'split("|") | {srpm: .[0], rpm: .[1]}')"
-json_data="$(echo "$json_srpms" | jq -s 'group_by(.srpm) | map({(.[0].srpm): map(.rpm)}) | add')"
+# rawdata="$(rpm -qa --queryformat '%{SOURCERPM}|%{NAME}\n' | grep -v "(none)")"
+# json_srpms="$(echo "$rawdata" | jq -R 'split("|") | {srpm: .[0], rpm: .[1]}')"
+# json_data="$(echo "$json_srpms" | jq -s 'group_by(.srpm) | map({(.[0].srpm): map(.rpm)}) | add')"
 
-total_rpms=0
-srpm_count=0
-for srpm in $(echo "$json_data" | jq -r 'keys[]'); do
-    file_count=0
-    rpms="$(echo "$json_data" | jq -r --arg srpm "$srpm" '.[$srpm][]')"
-    for rpm in $rpms; do
-        if [[ "$rpm" =~ kernel-core ]]; then
-            setfattr -n user.component -v "$srpm" "$(find /usr/lib/modules -type f -name initramfs.img)" 2>/dev/null || :
-            ((file_count+=1))
-        fi
-        for file in $(rpm -q --queryformat '%{FILENAMES}\n' "$rpm"); do
-            if [[ "$file" =~ .build-id ]]; then
-                continue
-            fi
-            if getfattr -n user.component "$file" &> /dev/null; then
-                continue
-            fi
-            if [[ -f "$file" || -d "$file" ]]; then
-                setfattr -n user.component -v "$srpm" "$file" 2>/dev/null || :
-                ((file_count+=1))
-            fi
-        done
-    ((total_rpms+=1))
-    done
-    echo "Processed $file_count files for $srpm"
-    ((srpm_count+=1))
-done
-echo "Processed $total_rpms RPMs across $srpm_count SRPMs"
+# total_rpms=0
+# srpm_count=0
+# for srpm in $(echo "$json_data" | jq -r 'keys[]'); do
+#     file_count=0
+#     rpms="$(echo "$json_data" | jq -r --arg srpm "$srpm" '.[$srpm][]')"
+#     for rpm in $rpms; do
+#         if [[ "$rpm" =~ kernel-core ]]; then
+#             setfattr -n user.component -v "$srpm" "$(find /usr/lib/modules -type f -name initramfs.img)" 2>/dev/null || :
+#             ((file_count+=1))
+#         fi
+#         for file in $(rpm -q --queryformat '%{FILENAMES}\n' "$rpm"); do
+#             if [[ "$file" =~ .build-id ]]; then
+#                 continue
+#             fi
+#             if getfattr -n user.component "$file" &> /dev/null; then
+#                 continue
+#             fi
+#             if [[ -f "$file" || -d "$file" ]]; then
+#                 setfattr -n user.component -v "$srpm" "$file" 2>/dev/null || :
+#                 ((file_count+=1))
+#             fi
+#         done
+#     ((total_rpms+=1))
+#     done
+#     echo "Processed $file_count files for $srpm"
+#     ((srpm_count+=1))
+# done
+# echo "Processed $total_rpms RPMs across $srpm_count SRPMs"
 
 find /usr /etc -type f -size +1M 2>/dev/null | while read -r file; do
     if ! rpm -qf "$file" &> /dev/null; then
