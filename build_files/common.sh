f63d24345ce7d12270346d679c022cfa3c43691f
m2 <69128853+m2Giles@users.noreply.github.com>
2026-07-04 15:42:25 -0400

    remove ostree container commit

--- build_files/common.sh
+++ build_files/common.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/bash
 
-trap_build="ostree container commit;"
-
 if [[ -n "${CI:-}" ]]; then
     WHAT="$(basename -s .sh "${BASH_SOURCE[1]}" | tr '-' ' ')"
     echo -e "::group::\e[1m\e[36m === ${WHAT^^} ===\e[0m"
-    trap_build+=" echo '::endgroup::';"
+    trap_build="echo '::endgroup::';"
 fi
 
 #shellcheck disable=SC2064
